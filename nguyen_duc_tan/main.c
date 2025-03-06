75b9bf5b9f93b14ad33e1d5c7224581d25914dc7
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-06 20:17:26 +0700

    tim max

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,5 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 void main() {
-	printf("Xin chao\n");
+	float diem;
+	printf("Nhap vao diem trung binh: ");
+	scanf("%f", &diem);
+	if (diem >= 8.0) {
+		printf("Hoc sinh gioi");
+	}
+	else if (8.0 > diem && diem >= 6.5) {
+		printf("Hoc sinh kha");
+	}
+	else if (6.5 > diem && diem >= 5.0) {
+		printf("Hoc sinh trung binh");
+	}
+	else if (diem < 5.0) {
+		printf("Hoc sinh yeu");
+	}
 }
