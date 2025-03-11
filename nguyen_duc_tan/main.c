361b837fc629598594e6990294b0c7b77b501b7d
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-11 10:25:45 +0700

    kt so duong/ so am

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -2,31 +2,16 @@
 #include<stdio.h>
 
 void main() {
-	float a, b, c, del,x, x1, x2;
-	printf("Nhap vao so a= ");
-	scanf("%f", &a);
-	printf("Nhap vao so b= ");
-	scanf("%f", &b);
-	printf("Nhap vao so c= ");
-	scanf("%f", &c);
-
-	if (a == 0) {
-		printf("Phuong trinh bac nhat");
+	int a;
+	printf("Nhap vao so  nguyen a= ");
+	scanf("%d", &a);
+	if (a > 0) {
+		printf("So %d la so duong\n", a);
+	}
+	else if (a < 0) {
+		printf("So %d la so am\n", a);
 	}
 	else {
-
-		del = b * b - 4 * a * c;
-		if (del < 0) {
-			printf("Phuong trinh vo nghiem");
-		}
-		else if (del == 0) {
-			x = -b / (2 * a);
-			printf("Phuong trinh co nghiem kep x1=x2= %f", x);
-		}
-		else {
-			x1 = (-b + sqrt(del)) / (2 * a);
-			x2 = (-b - sqrt(del)) / (2 * a);
-			printf("Phuong trinh co 2 nghiem phan biet x1= %f, x2= %f", x1, x2);
-		}
+		printf("So %d la so 0\n", a);
 	}
 }
