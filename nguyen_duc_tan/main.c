581b67a06aa3ff29f4f570121c8bc1f22c54a722
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-12 22:41:03 +0700

    tinh tien gui ngan hang sau n nam

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,21 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<math.h>
 
 void main() {
 	int n;
-	int m = 0;
-	int error = 0;
-	do {
-		printf("Nhap vao so: ");
-		scanf("%d", &n);
-		if (n != m) {
-			printf("Vui long nhap lai\n");
-			error++;
-			if (error == 5) {
-				break;
-			}
-		}
-	} while (n != m);
-
+	int so_tien_ban_dau;
+	int so_tien_lai = 0;
+	printf("Nhap so tien ban dau: ");
+	scanf("%d", &so_tien_ban_dau);
+	printf("Nhap so nam gui: ");
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		so_tien_lai = so_tien_ban_dau * pow((1 + 0.82), i);
+		printf("So tien sau %d nam la: %d\n", i, so_tien_lai);
+	}
 	
 }
