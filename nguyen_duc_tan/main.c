c4cad88e3eb0baf9d072e66fac9b184b439cd5b0
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-12 23:02:43 +0700

    in so nguyen to nho hon n

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -4,15 +4,15 @@
 
 void main() {
 	int n;
-	int so_tien_ban_dau;
-	int so_tien_lai = 0;
-	printf("Nhap so tien ban dau: ");
-	scanf("%d", &so_tien_ban_dau);
-	printf("Nhap so nam gui: ");
+	printf("Nhap vao so:");
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
-		so_tien_lai = so_tien_ban_dau * pow((1 + 0.82), i);
-		printf("So tien sau %d nam la: %d\n", i, so_tien_lai);
+		int dem = 0;
+		for (int j = 1; j <= i; j++) {
+			if (i%j == 0)
+				dem++;
+		}
+		if (dem == 2)
+			printf("%d ", i);
 	}
-	
 }
