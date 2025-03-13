c77db21a581efb7847f0a0952eee3165c96ab40c
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-13 18:17:37 +0700

    in cach doc so bang chu

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,36 +3,37 @@
 #include<math.h>
 
 void main() {
-	int so_tien_can_rut=0;
-	int so_to_50=0;
-	int so_to_100=0;
-	int so_to_200=0;
-	int so_to_500=0;
-	int so_tien_atm_tra = 0;
-	printf("Nhap so tien can rut: ");
-	scanf("%d", &so_tien_can_rut);
-	if (so_tien_can_rut % 50 != 0) {
-		printf("So tien can rut khong hop le");
-	}
-	else {
-		while (so_tien_can_rut > so_tien_atm_tra) {
-			if (so_tien_can_rut - so_tien_atm_tra >= 50)
-				so_to_50++;
-			so_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
-			if (so_tien_can_rut - so_tien_atm_tra >= 100)
-				so_to_100++;
-			so_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
-			if (so_tien_can_rut - so_tien_atm_tra >= 200)
-				so_to_200++;
-			so_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
-			if (so_tien_can_rut - so_tien_atm_tra >= 500)
-				so_to_500++;
-			so_tien_atm_tra = so_to_50 * 50 + so_to_100 * 100 + so_to_200 * 200 + so_to_500 * 500;
+	int dv, ch, tr, ng;
+	int n;
+	printf("Nhap so nguyen n: ");
+	scanf("%d", &n);
+	dv = n % 10;
+	ch = (n / 10) % 10;
+	tr = (n / 100) % 10;
+	ng = n / 1000;
+	int a;
+	for (int i = 0; i < 4; i++) {
+		if (i == 0) a = ng;
+		if (i == 1) a = tr;
+		if (i == 2) a = ch;
+		if (i == 3) a = dv;
+		switch (a) {
+		case 0: if (i == 1 && ng > 0) printf("Khong "); else if (i == 2 && dv > 0) printf("Le "); else if (n == 0) printf("Khong "); break;
+		case 1: printf("Mot "); break;
+		case 2: printf("Hai "); break;
+		case 3: printf("Ba "); break;
+		case 4: printf("Bon "); break;
+		case 5: printf("Nam "); break;
+		case 6: printf("Sau "); break;
+		case 7: printf("Bay "); break;
+		case 8: printf("Tam "); break;
+		case 9: printf("Chin "); break;
 		}
-		printf("So to 50: %d\n", so_to_50);
-		printf("So to 100: %d\n", so_to_100);
-		printf("So to 200: %d\n", so_to_200);
-		printf("So to 500: %d\n", so_to_500);
-		printf("So tien ATM tra: %d\n", so_tien_atm_tra);
+		switch (i) {
+		case 0: if (ng > 0) printf("Ngan "); break;
+		case 1: if (tr > 0 || (ng > 0 && (ch > 0 || dv > 0))) printf("Tram "); break;
+		case 2: if (ch > 0) printf("Muoi "); break;
+		case 3: printf("\n"); break;
+		}
 	}
 }
