e7cb2bfa4faee9d9be6bebf47bb2e0b8d4df9056
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-13 20:09:59 +0700

    tim cac vi tri cua so lon nhat

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,12 +3,16 @@
 #include<math.h>
 
 void main() {
-	int a[10];
-	int sum = 0;
+	int a[10] = { 1,3,5,7,9,1,9,4,1,6 };
+	int max = 0;
 	for (int i = 0; i < 10; i++) {
-		printf("Nhap vao a[%d]= ", i);
-		scanf("%d", &a[i]);
-		sum += a[i];
+		if (a[i] > max) {
+			max = a[i];
+		}
 	}
-	printf("Tong cua mang la: %d", sum);
+	for (int i = 0; i < 10; i++) {
+		if (a[i] == max) {
+			printf("Vi tri cua so lon nhat la %d\n", i);
+		}
+	}
 }
