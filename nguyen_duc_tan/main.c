909567187e90484d9a46ddd03b99131e7630f022
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-13 20:56:50 +0700

    dem ki tu

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,16 +3,11 @@
 #include<math.h>
 
 void main() {
-	int a[10] = { 1,3,5,7,9,1,9,4,1,6 };
-	int max = 0;
-	for (int i = 0; i < 10; i++) {
-		if (a[i] > max) {
-			max = a[i];
-		}
-	}
-	for (int i = 0; i < 10; i++) {
-		if (a[i] == max) {
-			printf("Vi tri cua so lon nhat la %d\n", i);
-		}
+	char str[] = "xin chao";
+	int c=0;
+	while (str[c]!='\0') {
+		printf("%c", str[c]);
+		c++;
+		printf(" : %d\n", c);
 	}
 }
