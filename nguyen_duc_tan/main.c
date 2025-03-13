844fa923a47a1afdfd66629ff5250e0f9ef3f4b5
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-13 19:53:02 +0700

    tinh tong cua mang

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,37 +3,12 @@
 #include<math.h>
 
 void main() {
-	int dv, ch, tr, ng;
-	int n;
-	printf("Nhap so nguyen n: ");
-	scanf("%d", &n);
-	dv = n % 10;
-	ch = (n / 10) % 10;
-	tr = (n / 100) % 10;
-	ng = n / 1000;
-	int a;
-	for (int i = 0; i < 4; i++) {
-		if (i == 0) a = ng;
-		if (i == 1) a = tr;
-		if (i == 2) a = ch;
-		if (i == 3) a = dv;
-		switch (a) {
-		case 0: if (i == 1 && ng > 0) printf("Khong "); else if (i == 2 && dv > 0) printf("Le "); else if (n == 0) printf("Khong "); break;
-		case 1: printf("Mot "); break;
-		case 2: printf("Hai "); break;
-		case 3: printf("Ba "); break;
-		case 4: printf("Bon "); break;
-		case 5: printf("Nam "); break;
-		case 6: printf("Sau "); break;
-		case 7: printf("Bay "); break;
-		case 8: printf("Tam "); break;
-		case 9: printf("Chin "); break;
-		}
-		switch (i) {
-		case 0: if (ng > 0) printf("Ngan "); break;
-		case 1: if (tr > 0 || (ng > 0 && (ch > 0 || dv > 0))) printf("Tram "); break;
-		case 2: if (ch > 0) printf("Muoi "); break;
-		case 3: printf("\n"); break;
-		}
+	int a[10];
+	int sum = 0;
+	for (int i = 0; i < 10; i++) {
+		printf("Nhap vao a[%d]= ", i);
+		scanf("%d", &a[i]);
+		sum += a[i];
 	}
+	printf("Tong cua mang la: %d", sum);
 }
