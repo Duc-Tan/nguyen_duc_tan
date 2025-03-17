74ba14021e2918360d254a8799157a202700e55e
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-17 21:43:42 +0700

    array: trung binh cong so le

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -5,10 +5,15 @@
 void main() {
 	int a[] = { 0,11,62,4,16,34,78,23,91,1,7 };
 	int sum = 0;
+	int dem = 0;
+	float tbc = 0;
 	int so_luong = sizeof(a) / sizeof(a[0]);
 	for (int i = 0; i < so_luong; i++) {
-		if (a[i] % 2 == 1)
-			sum+=a[i];
+		if (a[i] % 2 == 1) {
+			sum += a[i];
+			dem++;
+		}
 	}
-	printf("Tong cac so le trong mang la: %d\n", sum);
+	tbc = (float)sum / dem;
+	printf("Trung binh cong cac so le trong mang la: %f\n", tbc);
 }
