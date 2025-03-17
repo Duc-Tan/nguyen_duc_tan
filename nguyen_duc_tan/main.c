91f0e67bf85ea8feea8096f5e1f2f08e81e2a967
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-17 22:41:29 +0700

    string: dem ky tu

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,19 +3,11 @@
 #include<math.h>
 
 void main() {
-	int a[] = { 7,3,27,92,0,1,5,12,103,90,6,13,11 };
-	int so_luong = sizeof(a) / sizeof(a[0]);
-	
-	for (int i = 0; i < so_luong; i++) {
-		for (int j = i + 1; j < so_luong; j++) {
-			if (a[i] > a[j]) {
-				int temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
-	for (int i = 0; i < so_luong; i++) {
-		printf("%d ", a[i]);
+	char str[] = "xin chao";
+	int c = 0;
+	while (str[c] != 0) {
+		printf("%c", str[c]);
+		c++;
+		printf(" : %d\n", c);
 	}
 }
