c6fdf6ebba1328e095eca6849e3cd5d7579109dc
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-17 22:37:58 +0700

    array: Sap xep mang tang dan

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -4,12 +4,18 @@
 
 void main() {
 	int a[] = { 7,3,27,92,0,1,5,12,103,90,6,13,11 };
-	int max = 0;
 	int so_luong = sizeof(a) / sizeof(a[0]);
 	
 	for (int i = 0; i < so_luong; i++) {
-		if (max < a[i]) 
-			max = a[i];
+		for (int j = i + 1; j < so_luong; j++) {
+			if (a[i] > a[j]) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
 	}
-	printf("Gia tri lon nhat cua mang la: %d", max);
+	for (int i = 0; i < so_luong; i++) {
+		printf("%d ", a[i]);
+	}
 }
