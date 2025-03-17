fe0681ddf9f254b74e776cde440f4d0ef604deed
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-17 22:14:10 +0700

    array: cac so nguyen to cua mang

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,13 +3,18 @@
 #include<math.h>
 
 void main() {
-	int a[] = { 7,3,78,92,0,1,5,12,53,90,6,13,11 };
+	int a[] = { 7,3,27,92,0,1,5,12,53,90,6,13,11 };
 	int so_luong = sizeof(a) / sizeof(a[0]);
-	printf("Cac gia tri le trong mang la: ");
+	printf("Cac so nguyen to trong mang la: ");
 	for (int i = 0; i < so_luong; i++) {
-		if (a[i] % 2 == 1) {
+		int dem = 0;
+		for (int j = 1; j <= a[i]; j++) {
+			if (a[i] % j == 0) {
+				dem++;
+			}
+		}
+		if (dem == 2) {
 			printf("%d ", a[i]);
 		}
-		
 	}
 }
