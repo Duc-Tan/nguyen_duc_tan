54186c773c0e905ff65755a793a210999ad95a8c
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-17 22:17:29 +0700

    array: in GTLN cua mang

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,18 +3,13 @@
 #include<math.h>
 
 void main() {
-	int a[] = { 7,3,27,92,0,1,5,12,53,90,6,13,11 };
+	int a[] = { 7,3,27,92,0,1,5,12,103,90,6,13,11 };
+	int max = 0;
 	int so_luong = sizeof(a) / sizeof(a[0]);
-	printf("Cac so nguyen to trong mang la: ");
+	
 	for (int i = 0; i < so_luong; i++) {
-		int dem = 0;
-		for (int j = 1; j <= a[i]; j++) {
-			if (a[i] % j == 0) {
-				dem++;
-			}
-		}
-		if (dem == 2) {
-			printf("%d ", a[i]);
-		}
+		if (max < a[i]) 
+			max = a[i];
 	}
+	printf("Gia tri lon nhat cua mang la: %d", max);
 }
