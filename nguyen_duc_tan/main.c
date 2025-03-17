daa6b1afa18aeae15c57cc6f55401dd990e42862
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-17 21:37:56 +0700

    array: tong so le

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -3,11 +3,12 @@
 #include<math.h>
 
 void main() {
-	char str[] = "xin chao";
-	int c=0;
-	while (str[c]!='\0') {
-		printf("%c", str[c]);
-		c++;
-		printf(" : %d\n", c);
+	int a[] = { 0,11,62,4,16,34,78,23,91,1,7 };
+	int sum = 0;
+	int so_luong = sizeof(a) / sizeof(a[0]);
+	for (int i = 0; i < so_luong; i++) {
+		if (a[i] % 2 == 1)
+			sum+=a[i];
 	}
+	printf("Tong cac so le trong mang la: %d\n", sum);
 }
