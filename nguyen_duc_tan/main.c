250b1631e54136a96984337a0540a361db1d2b1a
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-20 12:56:34 +0700

    Xây dụng hàm. in ra các giá trị LẼ trong mảng arr

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,15 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
-
-void main() {
-	char str[] = "nguyen duc tan ";
-	int c = 0;
-	while (str[c] != 0) {
-		if (str[c] == 99) {
-			printf("Vi tri cua ki tu c la: %d\n", c);
-			printf("Dia chi cua ki tu c la: %p\n", &c);
+//in ra so le
+//input: nhap mang
+//output: khong co
+void in_so_le(int* dia_chi, int so_luong) {
+	for (int i = 0; i < so_luong; i++) {
+		if(*(dia_chi+i) % 2 != 0) {
+			printf("%d ", *(dia_chi+i));
 		}
-		c++;
 	}
+}
+void main() {
+	int a[] = {0, 7, 9, 6, 12, 17, 52, 90, 19};
+	in_so_le(a, 9);
 }
