c7779c4439dd138d7342996c9933e11e9ad239e2
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-25 20:55:58 +0700

    struct: tinh diem trung binh va xep loai

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,31 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-struct PhanSo {
-	int tu;
-	int mau;
-};
+// Xay dung kieu du lieu mo ta doi tuong hoc sinh
+// ten, tuoi, diem toan, diem van, diem trung binh (xep loai: 0=gioi, 1=kha, 2=trung binh, 3=yeu)
+typedef struct {
+	char* ten;
+	int tuoi;
+	float diemtoan;
+	float diemvan;
+	float diemtrungbinh;
+	int xeploai;
+} hocsinh;
 
-// Nhan 2 phan so
-// Input: 2 phan so A , B
-// Output: Phan so C = A * B
-struct PhanSo nhan(struct PhanSo A, struct PhanSo B) {
-	struct PhanSo C;
-	C.tu = A.tu * B.tu;
-	C.mau = A.mau * B.mau;
-	return C;
-};
+//Chuc nang: Tinh diem trung binh va xep loai hoc luc cua hoc sinh
+//Input: 
+// hocsinh* mang
+// int n: so luong hoc sinh
+//Output: khong co
+void tinhdiem(hocsinh* mang, int n) {
+	for (int i = 0; i < n; i++) {
+		mang[i].diemtrungbinh = (mang[i].diemtoan + mang[i].diemvan) / 2;
+		if (mang[i].diemtrungbinh >= 8) {
+			mang[i].xeploai = 0;
+		}
+		else if (mang[i].diemtrungbinh >= 6.5) {
+			mang[i].xeploai = 1;
+		}
+		else if (mang[i].diemtrungbinh >= 5) {
+			mang[i].xeploai = 2;
+		}
+		else {
+			mang[i].xeploai = 3;
+		}
+	}
+}
 
 void main() {
-	struct PhanSo A;
-	struct PhanSo B;
-
-	A.tu = 3;
-	A.mau = 2;
-
-	B.tu = 3;
-	B.mau = 5;
-
-	struct PhanSo C = nhan(A, B);
-	printf("%d/%d", C.tu, C.mau);
+	hocsinh hs[] = {
+		{"Nguyen Van A", 18, 8.5, 7.5, 0, 0},
+		{"Nguyen Van B", 18, 7.5, 8.5, 0, 0},
+		{"Nguyen Van C", 18, 6.5, 6.5, 0, 0},
+		{"Nguyen Van D", 18, 5.5, 5.5, 0, 0},
+		{"Nguyen Van E", 18, 4.5, 4.5, 0, 0}
+	};
+	tinhdiem(hs, 5);
+	for (int i = 0; i < 5; i++) {
+		printf("Ten: %s, Tuoi: %d, Diem toan: %.1f, Diem van: %.1f, Diem trung binh: %.1f, Xep loai: %d\n", hs[i].ten, hs[i].tuoi, hs[i].diemtoan, hs[i].diemvan, hs[i].diemtrungbinh, hs[i].xeploai);
+	}
 }
