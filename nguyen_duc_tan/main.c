f6fbb8396eb92281797b6c5e2fb67b50eb10c4c6
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-03-25 19:43:21 +0700

    nhan 2 phan so

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,15 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<math.h>
-#include "toan.h"
+
+struct PhanSo {
+	int tu;
+	int mau;
+};
+
+// Nhan 2 phan so
+// Input: 2 phan so A , B
+// Output: Phan so C = A * B
+struct PhanSo nhan(struct PhanSo A, struct PhanSo B) {
+	struct PhanSo C;
+	C.tu = A.tu * B.tu;
+	C.mau = A.mau * B.mau;
+	return C;
+};
 
 void main() {
-	int a = cong(2, 3);
-	int b = nhan(4, 11);
-	float c = chia(5, 2);
-	int d = tru(17, 5);
-	printf("a = %d\n", a);
-	printf("b = %d\n", b);
-	printf("c = %f\n", c);
-	printf("d = %d\n", d);
+	struct PhanSo A;
+	struct PhanSo B;
+
+	A.tu = 3;
+	A.mau = 2;
+
+	B.tu = 3;
+	B.mau = 5;
+
+	struct PhanSo C = nhan(A, B);
+	printf("%d/%d", C.tu, C.mau);
 }
