68be564911bf2cbee79ecdf12263f43232959db2
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-05-22 22:39:21 +0700

    Câu 4(làm lại): dự án về IoT yêu cầu nhận data từ server

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,44 +1,89 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
+typedef struct {
+	char light;
+	char fan;
+	char motor;
+}smartHome_t;
 
-int xu_ly_ham(char* str, char* key) {
-	char* p = strstr(str, key);// tim vi tri tu khoa trong chuoi
-	p += strlen(key);// di chuyen con tro den vi tri sau tu khoa
+const char* data = "HTTP1.1 200 OK{"\
+"\"light\": \"on\","\
+"\"fan\" : \"off\","\
+"\"motor\" : \"off\"}";
 
+smartHome_t pair_data(const char* data) {
+	smartHome_t x;
+	//light
+	char* light = "\"light\": \"";
+	char* pl = strstr(data, light);
+	pl += strlen(light);
 	int i = 0;
-	while (p[i] != ',') {
+	while (pl[i] != '\"') {
 		i++;
-	}//tim do dai cua tu khoa
-	char* key_state = malloc(i + 1);// cap phat bo nho cho chuoi con
-	memset(key_state, 0, i + 1);// khoi tao chuoi = 0
-	memcpy(key_state, p, i);// sao chep chuoi con tu vi tri p
+	}
+	char* light_state = malloc(i + 1);
+	memset(light_state, 0, i + 1);
+	memcpy(light_state, pl, i);
 
-	if (strcmp(key_state, "on") == 0) {
-		free(key_state);
-		return 1;
+	if (strcmp(light_state, "on") == 0) {
+		free(light_state);
+		x.light = 1;
 	}
-	else if (strcmp(key_state, "off") == 0) {
-		free(key_state);
-		return 0;
+	else if (strcmp(light_state, "off") == 0) {
+		free(light_state);
+		x.light = 0;
 	}
+	//fan
+	char* fan = "\"fan\" : \"";
+	char* pf = strstr(data, fan);
+	pf += strlen(fan);
+	int j = 0;
+	while (pf[j] != '\"') {
+		j++;
+	}
+	char* fan_state = malloc(j + 1);
+	memset(fan_state, 0, j + 1);
+	memcpy(fan_state, pf, j);
+
+	if (strcmp(fan_state, "on") == 0) {
+		free(fan_state);
+		x.fan = 1;
+	}
+	else if (strcmp(fan_state, "off") == 0) {
+		free(fan_state);
+		x.fan = 0;
+	}
+	//motor
+	char* motor = "\"motor\" : \"";
+	char* pm = strstr(data, motor);
+	pm += strlen(motor);
+	int z = 0;
+	while (pm[z] != '\"') {
+		z++;
+	}
+	char* motor_state = malloc(z + 1);
+	memset(motor_state, 0, z + 1);
+	memcpy(motor_state, pm, z);
+
+	if (strcmp(motor_state, "on") == 0) {
+		free(motor_state);
+		x.motor = 1;
+	}
+	else if (strcmp(motor_state, "off") == 0) {
+		free(motor_state);
+		x.motor = 0;
+	}
+	return x;
 }
 
+int main()
+{
+	smartHome_t x = pair_data(data);
+	int light = x.light;
+	int fan = x.fan;
+	int motor = x.motor;
 
-
-
-void main() {
-	char str[] = "light: on, fan: off, motor: off";
-	char light[] = "light: ";
-	char fan[] = "fan: ";
-	char motor[] = "motor: ";
-
-	int (*function)(char* str, char* key);
-	function = xu_ly_ham;
-
-	int light_state = xu_ly_ham(str, light);
-	int fan_state = xu_ly_ham(str, fan);
-	int motor_state = xu_ly_ham(str, motor);
+	return 0;
 }
