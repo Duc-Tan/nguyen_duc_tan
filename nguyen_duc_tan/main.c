41f3f459369a58ed4d46948d4f29f260e9da93b5
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-05-22 20:40:13 +0700

    Câu 4: dự án về IoT yêu cầu nhận data từ server

--- nguyen_duc_tan/main.c
+++ nguyen_duc_tan/main.c
@@ -1,21 +1,44 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-typedef union {
-	unsigned short x;
-	unsigned char y[2];
-}so_nguyen_t;
 
-void in_8bit_cao_8bit_thap(unsigned short val) {
-	so_nguyen_t a;
-	a.x = val;
-	printf("8 bit thap: %d\n", a.y[0]);
-	printf("8 bit cao: %d\n", a.y[1]);
+int xu_ly_ham(char* str, char* key) {
+	char* p = strstr(str, key);// tim vi tri tu khoa trong chuoi
+	p += strlen(key);// di chuyen con tro den vi tri sau tu khoa
+
+	int i = 0;
+	while (p[i] != ',') {
+		i++;
+	}//tim do dai cua tu khoa
+	char* key_state = malloc(i + 1);// cap phat bo nho cho chuoi con
+	memset(key_state, 0, i + 1);// khoi tao chuoi = 0
+	memcpy(key_state, p, i);// sao chep chuoi con tu vi tri p
+
+	if (strcmp(key_state, "on") == 0) {
+		free(key_state);
+		return 1;
+	}
+	else if (strcmp(key_state, "off") == 0) {
+		free(key_state);
+		return 0;
+	}
 }
 
+
+
+
 void main() {
-	unsigned short value;
-	printf("Nhap vao so nguyen 16 bit: ");
-	scanf("%hd", &value);
-	in_8bit_cao_8bit_thap(value);
+	char str[] = "light: on, fan: off, motor: off";
+	char light[] = "light: ";
+	char fan[] = "fan: ";
+	char motor[] = "motor: ";
+
+	int (*function)(char* str, char* key);
+	function = xu_ly_ham;
+
+	int light_state = xu_ly_ham(str, light);
+	int fan_state = xu_ly_ham(str, fan);
+	int motor_state = xu_ly_ham(str, motor);
 }
