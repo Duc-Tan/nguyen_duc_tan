a77db82aeef27741c62c44057a039121d726dd5f
DUC TAN <DUC TAN@DESKTOP-L107CJ5>
2025-05-22 19:12:14 +0700

    Xây dựng Class giải phương trình bậc 2

--- nguyen_duc_tan/main.cpp
+++ nguyen_duc_tan/main.cpp
@@ -1,70 +1,38 @@
-﻿#include<stdio.h>
-#include<iostream>
-#include<stdlib.h>
-
-class mang {
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+class phuongtrinhbac2{
+private:
+	float a;
+	float b;
+	float c;
 public:
-    int* data;
-    int size;
-    mang(int num_item) {
-        data = (int*)malloc(num_item * sizeof(int));
-        size = num_item;
-    }
-    int& operator[](int index) {
-        return data[index];
-    }
-
-    //Chuc nang: them gia tri moi vao cuoi mang
-    //Input: int val (gia tri moi)
-    //Output: khong co
-    void push_back(int val) {
-        //Cap phat vung nho moi lon hon
-        int* new_data = (int*)malloc((size + 1) * sizeof(int));
-        //Sao chep gia tri vung nho cu vao vung nho moi
-        for (int i = 0; i < size; i++) {
-            new_data[i] = data[i];
-        }
-        //Them gia tri moi vao mang
-        new_data[size] = val;
-        //Giai phong vung nho cu
-        free(data);
-        //Cap nhat dia chi cua vung nho moi vao con tro data
-        data = new_data;
-        //Cap nhat gia tri cua bien size
-        size++;
-    }
-    //Chuc nang: xoa gia tri cuoi mang
-    //Input: khong co
-    //Output: khong co
-    void pop_back() {
-        //Cap phat vung nho moi nho hon
-        int* new_data = (int*)malloc((size - 1) * sizeof(int));
-        //Sao chep gia tri vung nho cu vao vung nho moi
-        for (int i = 0; i < size - 1; i++) {
-            new_data[i] = data[i];
-        }
-        //Giai phong vung nho cu
-        free(data);
-        //Cap nhat dia chi cua vung nho moi vao con tro data
-        data = new_data;
-        //Cap nhat gia tri cua bien size
-        size--;
-    }
-
-    ~mang() {
-        free(data);
-    }
+	void nhap() {
+		cout << "Nhap a: ";
+		cin >> a;
+		cout << "Nhap b: ";
+		cin >> b;
+		cout << "Nhap c: ";
+		cin >> c;
+	}
+	void giai_pt_bac_2() {
+		float del;
+		del = b * b - 4 * a * c;
+		if(del < 0)
+			printf("Phuong trinh vo nghiem");
+		else if (del == 0) {
+			float x = -b / (2 * a);
+			printf("Phuong trinh co 1 nghiem: x= %f", x);
+		}
+		else if (del > 0) {
+			float x1 = (-b + sqrt(del)) / (2 * a);
+			float x2 = (-b - sqrt(del)) / (2 * a);
+			printf("Phuong trinh co 2 nghiem: x1= %f, x2= %f", x1, x2);
+		}
+	}
 };
-
-
-int main(void) {
-    mang a(5);
-    for (int i = 0; i < 5; i++) {
-        a[i] = i + 1;
-    }
-    a.pop_back();
-	a.push_back(6);
-	a.push_back(7);
-
-    return 0;
+void main() {
+	phuongtrinhbac2 fx;
+	fx.nhap();
+	fx.giai_pt_bac_2();
 }
